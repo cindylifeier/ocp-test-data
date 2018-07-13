f6ada5c1c97e272bea26f4bbcf4bc90ef830a200
Nisib.Sitaula <Nisib.Sitaula@feisystems.com>
2018-07-13 10:50:03 -0400

    Create a structure Definition helper.

---
+++ src/main/java/gov/samhsa/ocp/ocpfis/StructureDefinitionHelper.java
@@ -0,0 +1,69 @@
+package gov.samhsa.ocp.ocpfis;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import gov.samhsa.ocp.ocpfis.service.dto.ValueSetDto;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Slf4j
+public class StructureDefinitionHelper {
+
+    public static void process(){
+        Gson gson=new Gson();
+
+        File[] files=listFiles(new File(DataConstants.structureDefDir));
+
+        RestTemplate rt=new RestTemplate();
+
+        for(File file:files){
+            String fileName=file.getAbsolutePath();
+            log.info("file "+fileName);
+
+            try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))) {
+                JsonParser parser = new JsonParser();
+                parser.parse(br);
+
+            } catch (JsonSyntaxException | IOException e) {
+                e.printStackTrace();
+
+            }
+
+            log.info("Finished checking json validity ...");
+
+            try (BufferedReader br = Files.newBufferedReader(Paths.get(fileName))){
+                ValueSetDto valueSetDto=gson.fromJson(br,ValueSetDto.class);
+
+                log.info("Value retrieved and set in the object.");
+
+                HttpEntity<ValueSetDto> request=new HttpEntity<>(valueSetDto);
+                ResponseEntity<ValueSetDto> response=rt.exchange(getUrl(), HttpMethod.PUT, request, ValueSetDto.class);
+
+                log.info("response : "+response.getStatusCode());
+            } catch (IOException | JsonSyntaxException e){
+                e.printStackTrace();
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private static File[] listFiles(final File folder){
+        return folder.listFiles();
+    }
+
+    private static String getUrl(){
+        return DataConstants.serverUrl +"/structure-definitions";
+    }
+}
+
